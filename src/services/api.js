24ebbd5a3dac8edd6fa2bd5f86a396e69fb7c5b7
venkatasaimohit <venkatasaimohit12@gmail.com>
2026-05-11 09:46:27 +0530

    initial commit

---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+const API_KEY = "ab65a25f608f803a1384c154cfdd5067";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+
+  const data = await response.json();
+
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`,
+  );
+
+  const data = await response.json();
+
+  return data.results;
+};
